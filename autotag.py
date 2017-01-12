91fe17ea90b12f97ec512fe9e756242b02083762
Ulli Keuthe <ulli.keuthe@gmail.com>
2017-01-12 13:01:17 +0000

    added tagging support for rds

--- autotag.py
+++ autotag.py
@@ -108,9 +108,43 @@ class TaggableResource():
 
 
     def save_tags(self, new_tags_list):
-        self.resource.create_tags(Tags=new_tags_list)
+        try:
+            self.resource.create_tags(Tags=new_tags_list)
+        except:
+            logger.error("problem saving tags: "+str(sys.exc_info()[1])+" for "+self.id)
         
         
+class RDSTaggableResource(TaggableResource):
+
+    def __init__(self, id):
+        self.id = id
+        self.type = "RDS"
+        self.resource = None
+        self.client = boto3.client('rds')
+
+
+    def get_tags(self):
+        '''
+        returns the instances current tags as a simple dict i.e. {'Name': 'i-54543532', 'owner': 'teddy'}
+        '''
+        tags = {}
+        try:
+            response = self.client.list_tags_for_resource(ResourceName=self.id,Filters=[])
+            if 'TagList' in response:
+                return self._to_tag_dict(response['TagList'])
+            else:
+                return tags
+        except:
+            return tags
+
+
+    def save_tags(self, new_tags_list):
+        try:
+            response = self.client.add_tags_to_resource(ResourceName=self.id,Tags=new_tags_list)
+        except:
+            logger.error("problem saving tags: "+str(sys.exc_info()[1])+" for "+self.id)
+
+
 class S3TaggableResource(TaggableResource):
 
     def __init__(self, id):
@@ -136,28 +170,32 @@ class S3TaggableResource(TaggableResource):
 
 
     def save_tags(self, new_tags_list):
-        response = self.client.put_bucket_tagging(Bucket=self.id,Tagging={'TagSet': new_tags_list })
+        try:
+            response = self.client.put_bucket_tagging(Bucket=self.id,Tagging={'TagSet': new_tags_list })
+        except:
+            logger.error("problem saving tags: "+str(sys.exc_info()[1])+" for "+self.id)
 
 
 class CloudWatchEvent():
 
     def __init__(self,event):
+        self.tec_mail = None
         self.region = event['region']
         self.detail = event['detail']
         self.eventname = self.detail['eventName']
         self.arn = self.detail['userIdentity']['arn']
         self.principal = self.detail['userIdentity']['principalId']
-        self.assumedrole = self.detail['userIdentity']['sessionContext']['sessionIssuer']['userName']
-        self.creator = event['detail']['userIdentity']['principalId'].split(':')[1]
         self.userType = self.detail['userIdentity']['type']
         self.user = ''
         if self.userType == 'IAMUser':
             self.user = self.detail['userIdentity']['userName']
-        else:
-            self.user = self.principal.split(':')[1]
-
-        #if not self.detail['responseElements']:
-        #    raise RuntimeError('Not responseElements found')
+            self.assumedrole = self.detail['userIdentity']['userName']
+            self.tec_mail = None
+        elif self.userType == 'AssumedRole':
+            self.assumedrole = self.detail['userIdentity']['sessionContext']['sessionIssuer']['userName']
+            tmpuser = self.principal.split(':')[1]
+            self.user = tmpuser.split('@')[0].lower()
+            self.tec_mail = self.user+"@deutschebahn.com"
 
 
 class dbdal():
@@ -179,16 +217,19 @@ class dbdal():
 
 
     def query(self,key):
-        #self.db.quey(TableName=self.default_table, IndexName=assumed_role, Select='ALL_ATTRIBUTES'
-        #self.db.query(TableName=self.default_table, IndexName='assumed_role', Select='ALL_ATTRIBUTES')
         table = self.res.Table(self.default_table)
         try:
             response = table.query(KeyConditionExpression=Key('AssumedRole').eq(key))
+            logger.info("#1########### "+str(response)+" ##########################")
             return response
         except botocore.exceptions.ClientError as rex:
             if "ResourceNotFoundException" in str(rex):
                 # table not found -> create table
                 self.create_table()
+        except:
+            logger.error("#2###################################################")
+            logger.error(str(sys.exc_info()[1]))
+            
 
 
 def lambda_handler(event, context):
@@ -197,73 +238,81 @@ def lambda_handler(event, context):
     ec2 = boto3.resource('ec2')
 
     dal = dbdal()
-    res = dal.query(cwe.assumedrole)
-#    pprint.pprint(res, indent=4)
-#    try:
-#        if not res['ResponseMetadata']['HTTPStatusCode'] == 200 and res['Count'] == 1:
-#            logger.warn(
-#    
+    logger.info("database lookup with key: "+cwe.assumedrole)
+    # handle dynamo error here
+    try:
+        res = dal.query(cwe.assumedrole)
+        logger.error(res)
+        if not 'Count' in res:
+            logger.error("lookup with: "+cwe.assumedrole+" failed to return a valid response")
 
+        if res and res['Count'] == 0:
+            logger.notice("lookup with: "+cwe.assumedrole+" did not return information")
+            return False
+    except:
+        logger.error('failed to lookup on dynamo table')
+        logger.error(str(sys.exc_info()[1]))
+    
+        return False
+    
+    #print("####################################################### res ##############################")    
+    #pprint.pprint(res,indent=4)
+
+    new_tags = res['Items'][0]
+    if not '@' in new_tags['TechnicalContactMail']:
+        new_tags['TechnicalContactMail'] = cwe.tec_mail
+
+    # handle S3 ###################################################
     if cwe.eventname == 'CreateBucket':
         bucketname = cwe.detail['requestParameters']['bucketName']
         bucket = S3TaggableResource(bucketname)
         tags = bucket.get_tags()
-        #pprint.pprint(tags, indent=4)
-        bucket.add_tags({'ApplicationName': 'foobar'})
-
-        #tagS3Bucket(bucketname)
-        #logger.info(ids)
+        bucket.add_tags(new_tags)
+        return True
+    # handle RDS ##################################################
+    elif cwe.eventname == 'CreateDBInstance':
+        dbarn = cwe.detail['responseElements']['dBInstanceArn']
+        db = RDSTaggableResource(dbarn)
+        tags = db.get_tags()
+        db.add_tags(new_tags)
+        return True
+    # handle EBS ##################################################
     elif cwe.eventname == 'CreateVolume':
         ids.append(cwe.detail['responseElements']['volumeId'])
-        #logger.info(ids)
     elif cwe.eventname == 'CreateImage':
         ids.append(cwe.detail['responseElements']['imageId'])
-        #logger.info(ids)
     elif cwe.eventname == 'CreateSnapshot':
         ids.append(cwe.detail['responseElements']['snapshotId'])
-        #logger.info(ids)
+    # handle EC2 ##################################################
     elif cwe.eventname == 'RunInstances':
         items = cwe.detail['responseElements']['instancesSet']['items']
         for item in items:
             ids.append(item['instanceId'])
 
-            xx = TaggableResource(item['instanceId'])
-            xx.add_tags({'new_tag': 'new_value'})
-
         base = ec2.instances.filter(InstanceIds=ids)
         for instance in base:
-
-
             for vol in instance.volumes.all():
                 ids.append(vol.id)
             for eni in instance.network_interfaces:
                 ids.append(eni.id)
     else:
-        raise RuntimeError('unsupported action')
+        logger.error('unsupported action: '+cwe.eventname)
+        raise RuntimeError('unsupported action: '+cwe.eventname)
 
     # tagging the resources ##############################################################
     if ids:
         for resourceid in ids:
             logger.info('Tagging resource ' + resourceid)
             tr = TaggableResource(resourceid)
-            
-            #print(tr.get_tags())
-            #tr.add_tags({'foo': 'bar'})
-            print(tr.get_tags())
-
-            #tdict = to_tag_dict(instance.tags)
-            #tlist = to_tag_list(tdict)
-            #ec2.create_tags(Resources=ids, Tags=[{'Key': 'autotag:owner', 'Value': user}, {'Key': 'autotag:role', 'Value': assumedrole}])
-
-    #logger.info(' Remaining time (ms): ' + str(context.get_remaining_time_in_millis()) + '\n')
+            tr.add_tags(new_tags)
     return True
 
 
-
-
 if __name__ == "__main__":
+    #with open('rdsrun.json') as json_data:
     #with open('ec2run.json') as json_data:
-    with open('s3run.json') as json_data:
+    with open('ec2run.ask.json') as json_data:
+    #with open('s3run.json') as json_data:
         d = json.load(json_data)
         json_data.close()
         lambda_handler(d, 'context')    
